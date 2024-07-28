bd39b545684a9bc542dee4bd171fd5c840e08b37
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-07-28 20:39:52 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ let tg = window.Telegram.WebApp;
 
 tg.expand()
 
-let username = tg.initDataUnsafe.user.username;
-
-
 let profName = document.createElement('p'); //создаем параграф
 profName.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}
